5d36b43c93b9e9a7c63880bb68ab823812f3139a
alp <alperenmyung@gmail.com>
2021-08-04 17:40:22 +0200

    commit test

---
+++ components/MovieCard.jsx
@@ -0,0 +1,45 @@
+import React, {useEffect, useState} from 'react';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Carousel, Card} from 'react-bootstrap'
+import {RiStarSFill} from 'react-icons/ri'
+import {GiTrophyCup} from 'react-icons/gi'
+import {BiCameraMovie, BiMovie} from "react-icons/bi";
+
+
+
+function MovieCard(props) {
+    let data = props.data;
+    
+    let [image,setImage]= useState("");
+    let id = data.id;
+
+    useEffect(() => {
+        fetch(`http://www.omdbapi.com/?i=${id}&apikey=e4737124`).then(data=> data.json()).then(data => setImage(data.Poster))
+    }, [image])
+
+
+
+    return (
+ <>
+ <Card style={{height:"28rem", width:"25rem"}} >
+            <Card.Img variant="top" src= {image} style={{height:"12rem"}} />
+     <div style={{padding:"1rem 0 0 1.5rem"}}>
+         <p class="card-header-title">
+     {data.title} </p>
+    
+            <p> <GiTrophyCup style={{color:"orangered", margin:"0.2rem"}}/> IMDB Rank: <strong>{data.rank}</strong>
+            <span>
+            <RiStarSFill style= {{color:"orangered", margin:"0.2rem"}}/>Rating: <strong>{data.rating}</strong> </span> </p>
+            <p> <BiCameraMovie style= {{color:"orangered", margin:"0.2rem"}}/>Director: <strong>{data.directors}</strong></p>
+            <p style={{maxWidth:"30ch"}}>  <BiMovie style= {{color:"orangered", margin:"0.2rem"}}/>Actors: <strong>{data.stars}</strong></p>
+            
+
+       </div>     
+     </Card>       
+            
+</>
+    )
+}
+
+export default MovieCard;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import '../styles/globals.css'
+import Head from 'next/head'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+
+  return <>
+  <Head>
+  
+  <meta name="viewport" content="initial-scale=1.0" />
+  
+    </Head>
+  
+  
+  <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
